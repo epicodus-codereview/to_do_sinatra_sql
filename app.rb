12a698e69546afc155c4716b1de5f90a547b0d88
Mike Goren <epicodus-codereview@mortalwombat.net>
2015-08-20 22:30:02 -0700

    comment app.rb to help explain restful routing

--- app.rb
+++ app.rb
@@ -11,15 +11,20 @@ get("/") do
   erb(:index)
 end
 
+##### LIST RESTFUL ROUTES #####
+
+# List 'index' RESTful route (display a list of all lists)
 get('/lists') do
   @lists = List.all()
   erb(:lists)
 end
 
+# List 'new' RESTful route (show form to gather info for creating a new list)
 get("/lists/new") do
   erb(:list_form)
 end
 
+# List 'create' RESTful route (create a new list)
 post("/lists") do
   name = params.fetch("name")
   list = List.new({:name => name, :id => nil})
@@ -27,11 +32,37 @@ post("/lists") do
   erb(:list_success)
 end
 
+# List 'show' RESTful route (display detail page for a specific list)
 get("/lists/:id") do
   @list = List.find(params.fetch("id").to_i())
   erb(:list)
 end
 
+# List 'edit' RESTful route (show form to gather info for editing a list)
+get("/lists/:id/edit") do
+  @list = List.find(params.fetch("id").to_i())
+  erb(:list_edit)
+end
+
+# List 'update' RESTful route (update a specific list)
+patch("/lists/:id") do
+  name = params.fetch("name")
+  @list = List.find(params.fetch("id").to_i())
+  @list.update({:name => name})
+  erb(:list)
+end
+
+# List 'delete' RESTful route (delete a specific list)
+delete("/lists/:id") do
+  @list = List.find(params.fetch("id").to_i())
+  @list.delete()
+  @lists = List.all()
+  erb(:index)
+end
+
+##### TASK RESTFUL ROUTES #####
+
+# Task 'create' RESTful route (create a new task)
 post("/tasks") do
   description = params.fetch("description")
   list_id = params.fetch("list_id").to_i()
@@ -41,21 +72,3 @@ post("/tasks") do
   erb(:task_success)
 end
 
-get("/lists/:id/edit") do
-  @list = List.find(params.fetch("id").to_i())
-  erb(:list_edit)
-end
-
-patch("/lists/:id") do
-  name = params.fetch("name")
-  @list = List.find(params.fetch("id").to_i())
-  @list.update({:name => name})
-  erb(:list)
-end
-
-delete("/lists/:id") do
-  @list = List.find(params.fetch("id").to_i())
-  @list.delete()
-  @lists = List.all()
-  erb(:index)
-end
